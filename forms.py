232db1709a329f094ea2a54e4bdd637eab9611c3
Moh <mohrichmond@gmail.com>
2024-08-17 07:08:34 +0100

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField, PasswordField, validators, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, URL, Email
+from flask_ckeditor import CKEditorField
+
+
+
+
---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+
+
+app = Flask(__name__)
+
+Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about, methods=['GET']")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact, methods=['GET']")
+def contact():
+    return render_template("contact.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port="5000")
